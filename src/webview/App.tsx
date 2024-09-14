5e4d5d6535ef6bf58b8a8425a0f29fa976230b2a
HarlenPostill <harlen.postill@student.uts.edu.au>
2024-09-14 14:39:16 +1000

    Added accessibility score template with cleaned up code

--- src/webview/App.tsx
+++ src/webview/App.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Header from './components/Header';
 import ActionItems from './components/ActionItems/ActionItems';
 import AccessibilityScore from './components/AccessibilityScore/AccessibilityScore';
 
 const vscode = acquireVsCodeApi();
 
-const App = () => {
+const App: React.FC = () => {
+  const [width, setWidth] = useState(0);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  // Update width whenever the window is resized for designers
+  useEffect(() => {
+    const updateWidth = () => {
+      if (divRef.current) {
+        setWidth(divRef.current.offsetWidth);
+      }
+    };
+    updateWidth();
+    window.addEventListener('resize', updateWidth);
+    return () => {
+      window.removeEventListener('resize', updateWidth);
+    };
+  }, []);
+
   return (
-    <div>
+    <div ref={divRef}>
       <Header title="Welcome to GuideDog" />
       <ActionItems vscode={vscode} />
       <AccessibilityScore />
+
+      {/* DEV ONLY TO SEE WIDTH */}
+      <div style={{ marginTop: '1em', color: '#666' }}>
+        Current width: {width}px, Ideal is 343px
+      </div>
     </div>
   );
 };
--- src/webview/components/AccessibilityScore/AccessibilityScore.tsx
+++ src/webview/components/AccessibilityScore/AccessibilityScore.tsx
@@ -1,40 +1,12 @@
 import React from 'react';
 import Divider from '../Divider/Divider';
+import './AccessibilityScore.css';
 
 const AccessibilityScore = () => {
   return (
-    <div
-      style={{
-        width: '100%',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        gap: 15,
-        display: 'inline-flex',
-        marginBottom: '0.67em'
-      }}
-    >
-      <div
-        style={{
-          color: '#CCCCCC',
-          fontSize: 18,
-          fontWeight: '700',
-          wordWrap: 'break-word'
-        }}
-      >
-        Accessibility Score:
-      </div>
-
-      <div
-        style={{
-          color: '#71BBFF',
-          fontWeight: '700',
-          wordWrap: 'break-word',
-          fontSize: '19vw'
-        }}
-      >
-        365/400
-      </div>
+    <div className="accessibility-score-container">
+      <div className="accessibility-score-label">Accessibility Score:</div>
+      <div className="accessibility-score-value">365/400</div>
       <Divider />
     </div>
   );
