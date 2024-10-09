bf6b5b0d6f4349b1eb6d3b1a1100cfecefdc002d
HarlenPostill <harlen.postill@student.uts.edu.au>
2024-10-09 20:33:00 +1100

    added skeleton for issue list

--- src/webview/components/Organisms/RepoIssuesList/RepoIssuesList.tsx
+++ src/webview/components/Organisms/RepoIssuesList/RepoIssuesList.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import './RepoIssuesList.css';
 import Link from '../../Atoms/Link/Link';
 import { useDictionary } from '../../../hooks/useDictionary';
+import PetsIcon from '@mui/icons-material/Pets';
+import SwapVertOutlinedIcon from '@mui/icons-material/SwapVertOutlined';
 
-const RepoIssuesList = () => {
+interface RepoIssuesListProps {
+  hasSort?: boolean;
+}
+
+const RepoIssuesList = ({ hasSort = false }: RepoIssuesListProps) => {
   const d = useDictionary();
 
   return (
     <div>
       <div className="headerGroup">
-        <div className="title">{d('ui.boxes.issueList.title')}</div>
+        <div className="title">
+          {d('ui.boxes.issueList.title')}
+          {hasSort && <SwapVertOutlinedIcon sx={{ fontSize: 19 }} />}
+        </div>
         <Link
           name={d('ui.links.history')}
           hasIcon={true}
@@ -17,6 +26,19 @@ const RepoIssuesList = () => {
             throw new Error('Function not implemented.');
           }}
         />
+      </div>
+      <div className="issuesContainer">
+        <div className="issueGroup">
+          <div className="issueHeader">
+            <div className="issueTitle">
+              <PetsIcon sx={{ fontSize: 12 }} htmlColor="FF6D6D" />
+              <div>Missing alt text </div>
+            </div>
+            <div className="issuePill">5 issues</div>
+          </div>
+          <span>random text</span>
+          <span>random text 2</span>
+        </div>
       </div>
     </div>
   );
