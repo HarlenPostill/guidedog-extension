fb119c5fbe5c958651b6892d7e43408dfaab7da1
HarlenPostill <harlen.postill@student.uts.edu.au>
2024-10-11 12:59:24 +1100

    small bug fixes

--- src/webview/components/Molecules/StatusIndicator/StatusIndicator.tsx
+++ src/webview/components/Molecules/StatusIndicator/StatusIndicator.tsx
@@ -5,42 +5,40 @@ import { useDictionary } from '../../../hooks/useDictionary';
 
 interface StatusIndicatorProps {
   percentage?: number;
-  lastUpdated: string; 
+  lastUpdated: string;
 }
 
-const StatusIndicator: React.FC<StatusIndicatorProps> = ({ percentage, lastUpdated }) => {
+const StatusIndicator = ({ percentage, lastUpdated }: StatusIndicatorProps) => {
   let status = '';
   let colorClass = '';
-if(percentage){
-  
-  if (percentage <= 25) {
-    status = 'Perceivable';
-    colorClass = 'perceivable';
-  } else if (percentage <= 50) {
-    status = 'Operable';
-    colorClass = 'operable';
-  } else if (percentage <= 75) {
-    status = 'Understandable';
-    colorClass = 'understandable';
-  } else {
-    status = 'Critical';
-    colorClass = 'critical';
+  if (percentage) {
+    if (percentage <= 25) {
+      status = 'Perceivable';
+      colorClass = 'perceivable';
+    } else if (percentage <= 50) {
+      status = 'Operable';
+      colorClass = 'operable';
+    } else if (percentage <= 75) {
+      status = 'Understandable';
+      colorClass = 'understandable';
+    } else {
+      status = 'Critical';
+      colorClass = 'critical';
+    }
   }
-}
 
-const d = useDictionary();
+  const d = useDictionary();
 
   return (
-    <div
-      className={`status-indicator ${colorClass}`}>
+    <div className={`status-indicator ${colorClass}`}>
       <div className="status">
-        <span>{status}</span>
+        <span>{d(`ui.headers.status.${status}`)}</span>
         <span className="paw-icon">
           <Icon name="Paw" width={24} height={24} />
         </span>
       </div>
       <div className="last-updated">
-        {d("ui.text.lastUpdated")} <span>{lastUpdated}</span> {/* Use the lastUpdated prop */}
+        {d('ui.text.lastUpdated')} <span>{lastUpdated}</span>
         <button className="refresh-button">
           <Icon name="RefreshIcon" width={24} height={24} style={{ fill: 'white' }} />
         </button>
