03cc21fba2334406bbb007234aa9a6d6cdd8fd08
HarlenPostill <harlen.postill@student.uts.edu.au>
2024-10-12 16:04:58 +1100

    Added browser storage to store language

--- src/webview/hooks/LanguageContext.tsx
+++ src/webview/hooks/LanguageContext.tsx
@@ -18,8 +18,13 @@ type LanguageProviderProps = {
   children: ReactNode;
 };
 
+const defaultLanguage: Language = { code: 'EN', name: 'English' };
+
 export const LanguageProvider: React.FC<LanguageProviderProps> = ({ children }) => {
-  const [currentLanguage, setCurrentLanguage] = useState<Language>({ code: 'EN', name: 'English' });
+  const [currentLanguage, setCurrentLanguage] = useState<Language>(() => {
+    const savedLanguage = localStorage.getItem('preferredLanguage');
+    return savedLanguage ? JSON.parse(savedLanguage) : defaultLanguage;
+  });
   const [dictionary, setDictionary] = useState<{ [key: string]: string }>({});
 
   useEffect(() => {
@@ -27,6 +32,9 @@ export const LanguageProvider: React.FC<LanguageProviderProps> = ({ children }) 
     import(`../dictionaries/${currentLanguage.code.toLowerCase()}.json`)
       .then(module => setDictionary(module.default))
       .catch(error => console.error('Error loading dictionary:', error));
+
+    // Save the current language to localStorage
+    localStorage.setItem('preferredLanguage', JSON.stringify(currentLanguage));
   }, [currentLanguage]);
 
   const setLanguage = (lang: Language) => {
