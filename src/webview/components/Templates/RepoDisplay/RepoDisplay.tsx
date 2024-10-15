36058a1c3e6abbec28ffc2e94e4eb48faaf7bd82
angelovvv <felicialo888@gmail.com>
2024-10-15 19:29:44 +1100

    Fixed percentage graph
    remove redundant containers, fixed the circle text wrapping, fixed the pie chart from shrinking, readjust styling.

--- src/webview/components/Templates/RepoDisplay/RepoDisplay.tsx
+++ src/webview/components/Templates/RepoDisplay/RepoDisplay.tsx
@@ -8,7 +8,7 @@ import ScoreBreakdown from '../../Molecules/ScoreBreakdown/ScoreBreakdown';
 const RepoDisplay = (vscode: any) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
-      <ScoreBreakdown perc={10} oper={25} unde={20} robu={30} />
+      <ScoreBreakdown perc={20} oper={40} unde={30} robu={10} />
       <Divider />
       <ViolationsOverview A={100} AA={50} AAA={100} />
       <Divider />
